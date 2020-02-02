d50c25236bc43892d14a0d2bfbb2eb18cb3fc69f
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-02-02 14:10:58 +0100

    Added Prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,10 @@
  * Read more: https://eslint.org/docs/user-guide/configuring
  */
 module.exports = {
-  extends: 'react-app'
+  extends: [
+    'react-app',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
 }
