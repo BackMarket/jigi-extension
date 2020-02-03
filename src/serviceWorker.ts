3057d4143518e9addb61a60eb82d56ac42458c1e
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-02-03 09:37:41 +0100

    Ignored no-param-reassign

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -29,6 +29,8 @@ function registerValidSW(swUrl: string, config?: Config) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
+      // Assignment to property is required on this API
+      // eslint-disable-next-line no-param-reassign
       registration.onupdatefound = () => {
         const installingWorker = registration.installing
         if (installingWorker == null) {
