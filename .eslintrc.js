19c72f9f2e2705c2dd102eb3715699d60f820900
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-02-11 05:50:39 +0100

    chore(linting): Setup linting (#5)

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+/**
+ * ESLint configuration
+ * Read more: https://eslint.org/docs/user-guide/configuring
+ */
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:import/typescript',
+    'plugin:jest/all',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.{spec,test}.{js,jsx,ts,tsx}',
+          'src/setupTests.ts',
+        ],
+      },
+    ],
+    'jest/prefer-expect-assertions': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+  },
+  overrides: [
+    {
+      files: 'src/**',
+      env: { browser: true },
+    },
+    {
+      files: '**/*.d.ts',
+      rules: {
+        // Fixes spaced-comment - Expected exception block, space or tab after '//' in comment
+        // Read more: https://github.com/typescript-eslint/typescript-eslint/issues/600#issuecomment-499979248
+        'spaced-comment': ['error', 'always', { markers: ['/'] }],
+      },
+    },
+  ],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/**
+ * Prettier configuration file.
+ * For more details read https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
